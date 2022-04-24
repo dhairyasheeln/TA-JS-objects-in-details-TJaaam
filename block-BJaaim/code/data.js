d97144675605d7fe11b0c4d5d157e09cbc04ff28
dhairyasheel <dhairyasheeln@gmail.com>
2022-04-24 09:41:14 +0530

    Quiz App final

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,59 @@
+const quizCollection=[
+    {
+        title:'What temperatur does water boil at?',
+        options:[
+            '50 degree Celcius',
+            '25 degree Celcius',
+            '100 degree Celcius',
+            '150 degree Celcius',
+        ],
+        answerIndex:2,
+    },
+
+    {
+        title:'Which animal is known as the Ship of the Desert?',
+        options:[
+            'Camel',
+            'Elephant',
+            'Turtle',
+            'Eagle',
+        ],
+        answerIndex:0,
+    },
+
+
+    {
+        title:'Baby frog is known as.......',
+        options:[
+            'Serim',
+            'Polium',
+            'Tadpole',
+            'Nemo',
+        ],
+        answerIndex:2,
+    },
+
+
+    {
+        title:'Name the National fruit of India?',
+        options:[
+            'Watermelon',
+            'Mango',
+            'Jackfruit',
+            'Pineapple',
+        ],
+        answerIndex:1,
+    },
+
+
+    {
+        title:'Name the National tree of India?',
+        options:[
+            'Sandal',
+            'Shisham',
+            'Chir pine',
+            'Banyan Tree',
+        ],
+        answerIndex:3,
+    },
+]
